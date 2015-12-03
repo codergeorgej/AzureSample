6dd3dff9db70542bf0b50297caa5b32ad74fdf1e
George Jacob <georgej@datacom.co.nz>
2015-12-03 14:50:51 +1300

    code change

--- AzureSample/Controllers/HomeController.cs
+++ AzureSample/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AzureSample.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page testinnnnnnnn.";
 
             return View();
         }
